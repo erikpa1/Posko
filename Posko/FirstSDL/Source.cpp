b4320e0aedbecb4e6a2bc8456e92f451047ba673
Erik Palenčík <erikpa1@azet.sk>
2018-11-19 18:36:40 +0100

    netusim co updatujem

--- Posko/FirstSDL/Source.cpp
+++ Posko/FirstSDL/Source.cpp
@@ -1,15 +1,20 @@
 #pragma once
 #include "Game.h"
+#include <iostream>
 
 
 int main(int argc, char* argv[]) {
+
+	std::string mystring;
+	mystring.resize(100);
+	std::cout << "My string" << mystring[20];
 
 	Game game;
 	game.Construct();
 	game.Start();
 	game.Destruct();
 
-
+	
 
 	
 	return 0;
