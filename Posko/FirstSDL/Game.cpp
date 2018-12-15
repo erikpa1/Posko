2ac8476d9182cdbcd4349662cb91d30b346db118
Erik Palenčík <erikpa1@azet.sk>
2018-12-15 19:58:58 +0100

    pridanie net library

--- Posko/FirstSDL/Game.cpp
+++ Posko/FirstSDL/Game.cpp
@@ -40,17 +40,24 @@ void Game::Construct()
 	InitPlayer(_players[0]);
 	InitPlayer(_players[1]);
 
+	_ball = new Ball();
+	_ball->Construct();
+	_ball->SetName("Game ball");
+	_ball->setW(10);
+	_ball->setH(10);
+
+	_ball->setX(_gameWindow->getH() / 2 - _ball->getH() / 2);
+	_ball->setY(_gameWindow->getW() / 2 - _ball->getW() / 2);
+
 	_gameWindow->AddChildren(_players[0]);
 	_gameWindow->AddChildren(_players[1]);
-
+	_gameWindow->AddChildren(_ball);
 	_gameWindow->AddEventRecievers(this);
 		
 	_players[0]->setX(0);	
 	_players[1]->setX(_gameWindow->getW() - _players[1]->getW());
 
-	_ball = new Ball();
-	_ball->Construct();
-	_ball->SetName("Game ball");	
+
 }
 
 void Game::Destruct()
@@ -126,82 +133,4 @@ void Game::ChangePosition(bool up, int amount, int playerChoose)
 			helper->setY(helper->getY() + PLAYER_MOVEMENT);
 		}
 	}
-}
-
-void Game::ballMovement(int direction)
-{
-	switch (direction)
-	{
-	case UPRIGHT:
-		_ball->setY(_ball->getY() - BALL_MOVEMENT);
-		_ball->setX(_ball->getX() + BALL_MOVEMENT);
-		break;
-	case RIGHT:
-		_ball->setX(_ball->getX() + BALL_MOVEMENT);
-		break;
-	case DOWNRIGHT:
-		_ball->setY(_ball->getY() + BALL_MOVEMENT);
-		_ball->setX(_ball->getX() + BALL_MOVEMENT);
-		break;
-	case  DOWNLEFT:
-		_ball->setY(_ball->getY() + BALL_MOVEMENT);
-		_ball->setX(_ball->getX() - BALL_MOVEMENT);
-		break;
-	case LEFT:
-		_ball->setX(_ball->getX() - BALL_MOVEMENT);
-		break;
-	case UPLEFT:
-		_ball->setY(_ball->getY() + BALL_MOVEMENT);
-		_ball->setX(_ball->getX() - BALL_MOVEMENT);
-		break;
-	default:
-		break;
-	}
-
-}
-
-void Game::detectColision()
-{
-	//hit by left player
-	if (_ball->getX() - BALL_MOVEMENT <= _players[0]->getX() + _players[0]->getW() &&
-		_ball->getY() >= _players[0]->getY() && _ball->getY() <= _players[0]->getY()+_players[0]->getH())
-	{
-		//change direction here
-	}
-	//hit by right player
-	if (_ball->getX() + _ball->getW() + BALL_MOVEMENT >= _players[1]->getX() &&
-		_ball->getY() >= _players[1]->getY() && _ball->getY() <= _players[1]->getY() + _players[1]->getH())
-	{
-		//change direction here
-	}
-	//hit by left wall
-	if (_ball->getX() - BALL_MOVEMENT <= STARTOFWINDOW)
-	{
-		//add score to right player
-		resetBallPosition();
-	}
-	//hit by right wall
-	if (_ball->getX() + _ball->getW() + BALL_MOVEMENT >= _gameWindow->getW())
-	{
-		//add score to left player
-		resetBallPosition();
-	}
-	//hit by top wall
-	if (_ball->getY() <= STARTOFWINDOW)
-	{
-		//change direction here
-	}
-	//hit by bottom wall
-	if (_ball->getY() >= _gameWindow->getH())
-	{
-		//change direction here
-	}
-}
-
-
-
-void Game::resetBallPosition()
-{
-	_ball->setX(_gameWindow->getW() / 2);
-	_ball->setY(_gameWindow->getH() / 2);
 }
