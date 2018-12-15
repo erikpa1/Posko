57341fa2468ce827f2cc0cd6df62f13c57e2962c
Baatun <stolicnysamuel@gmail.com>
2018-12-15 21:24:04 +0100

    Rakšupšlapka

--- ServerSide/AppControll.cpp
+++ ServerSide/AppControll.cpp
@@ -4,7 +4,7 @@
 #include "Player.h"
 
 #define PLAYER_MOVEMENT 10
-#define BALL_MOVEMENT 2
+#define BALL_MOVEMENT 10
 #define	STARTOFWINDOW 0
 
 //ball movement directions
@@ -19,10 +19,11 @@
 void AppControll::Construct()
 {
 	_socket = new Socket(123456);
-	_socket->Consturct();
+	//_socket->Consturct();
 	_players.push_back(new Player());
 	_players.push_back(new Player());
 	_ball = new Ball();
+        _ball->setLeft(true);
 
 	_players[0]->Construct();
 	_players[1]->Construct();
@@ -47,14 +48,22 @@ void AppControll::Start()
 {
 	while (true)
 	{
-		cout << "Server started running cycle" << endl;
+		//cout << "Server started running cycle" << endl;
 
 		//ReadFromClinet();
+            cout << "----------------------------------------" << endl;
 		Update();
+                _ball->setY(265);
+                cout << "Pozicia Lopticky" << endl;
+                _ball->PrintYourSelf();
+                cout << "pozicia laveho hraca" << endl;
+                _players[0]->PrintYourSelf();
+                cout << "pozicia praveho hraca" << endl;
+                _players[1]->PrintYourSelf();
 		//SendToClient();
 
 
-		cout << "Server ended running running cycle" << endl;
+		//cout << "Server ended running running cycle" << endl;
 	}
 }
 
@@ -95,18 +104,21 @@ void AppControll::ResetBallPosition()
 
 void AppControll::UpdateBallPosition()
 {
+    
+    //_ball->setY(_ball->getY() + BALL_MOVEMENT);
+    
     if (_ball->getUp())
     {
-        _ball->setY(_ball->getY() + BALL_MOVEMENT);
+        //_ball->setY(_ball->getY() + BALL_MOVEMENT);
     } else {
-        _ball->setY(_ball->getY() - BALL_MOVEMENT);
+        //_ball->setY(_ball->getY() - BALL_MOVEMENT);
     }
     
     if (_ball->getLeft())
     {
-        _ball->setX(_ball->getY() - BALL_MOVEMENT);
+        _ball->setX(_ball->getX() - BALL_MOVEMENT);
     } else {
-        _ball->setY(_ball->getY() - BALL_MOVEMENT);
+        _ball->setX(_ball->getX() + BALL_MOVEMENT);
     }    
 }
 
@@ -116,44 +128,36 @@ void AppControll::DetectCollision()
     if (_ball->getX() - BALL_MOVEMENT <= _players[0]->getX() + _players[0]->getW() &&
     _ball->getY() >= _players[0]->getY() && _ball->getY() <= _players[0]->getY()+_players[0]->getH())
     {
-        _ball->setRight(true);
+        //_ball->setRight(true);
         _ball->setLeft(false);
-    }
-    
-    //hit by right player
-    if (_ball->getX() + _ball->getW() + BALL_MOVEMENT >= _players[1]->getX() &&
-    _ball->getY() >= _players[1]->getY() && _ball->getY() <= _players[1]->getY() + _players[1]->getH())
+    } else  if (_ball->getX() + _ball->getW() + BALL_MOVEMENT >= _players[1]->getX() &&
+        _ball->getY() >= _players[1]->getY() && _ball->getY() <= _players[1]->getY() + _players[1]->getH())
     {
-        _ball->setRight(false);
+        //hit by right player
+        //_ball->setRight(false);
         _ball->setLeft(true);
-    }
-    
-    //hit by left wall
-    if (_ball->getX() - BALL_MOVEMENT <= STARTOFWINDOW)
+    } else if (_ball->getX() - BALL_MOVEMENT <= STARTOFWINDOW)
     {
+        //hit by left wall
         _score_right += 1;
         UpdateScoreOnClients();
         ResetBallPosition();
-    }
-    
-    //hit by right wall
-    if (_ball->getX() + _ball->getW() + BALL_MOVEMENT >= _h)
+        cout << "Hit by Left wall" << endl;
+    } else if (_ball->getX() + _ball->getW() + BALL_MOVEMENT >= _h)
     {
+        //hit by right wall
         _score_left += 1;
         UpdateScoreOnClients();
         ResetBallPosition();
-    }
-    
-    //hit by top wall
-    if (_ball->getY() <= STARTOFWINDOW)
+        cout << "Hit by Right wall" << endl;
+    } else if (_ball->getY() <= STARTOFWINDOW)
     {
+        //hit by top wall
         _ball->setUp(false);
         _ball->setDown(true);
-    }
-    
-    //hit by bottom wall
-    if (_ball->getY() >= _h)
+    }else if (_ball->getY() >= _h)
     {
+        //hit by bottom wall
         _ball->setUp(true);
         _ball->setDown(false);
     }               
